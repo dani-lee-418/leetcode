e0c8423f8a4e0f60d691ea14beb9b69969b08164
dani-lee-418 <141555426+dani-lee-418@users.noreply.github.com>
2023-10-21 14:42:01 +0900

    Create InvertBinaryTree_266.java

---
+++ src/week5/InvertBinaryTree_266.java
@@ -0,0 +1,45 @@
+package week5;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class InvertBinaryTree_266 {
+
+    public static void main(String[] args) {
+        InvertBinaryTree_266 a = new InvertBinaryTree_266();
+//        TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(2), new TreeNode(3)), new TreeNode(7, new TreeNode(6), new TreeNode(9)));
+        TreeNode root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+        a.invertTree(root);
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        } else {
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            invertTree(root.right);
+            invertTree(root.left);
+        }
+        return root;
+    }
+
+
+}
